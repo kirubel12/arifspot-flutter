915d2d9512dc2b310cfc9f8d835b19caae0dbca8
kirubel12 <kirubelhabte1991@gmail.com>
2025-03-07 05:58:32 -0800

    feat: add popular accomodation

--- lib/components/popular__accomodation.dart
+++ lib/components/popular__accomodation.dart
@@ -1,7 +1,20 @@
+import 'package:arifspot/constants/color.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class PopularAccomodation extends StatefulWidget {
-  const PopularAccomodation({super.key});
+  final String name;
+  final double rating;
+  final String image;
+  final String location;
+
+  const PopularAccomodation({
+    super.key,
+    required this.name,
+    required this.rating,
+    required this.image,
+    required this.location,
+  });
 
   @override
   State<PopularAccomodation> createState() => _PopularAccomodationState();
@@ -10,6 +23,61 @@ class PopularAccomodation extends StatefulWidget {
 class _PopularAccomodationState extends State<PopularAccomodation> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: 243,
+      height: 290,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 207,
+            height: 175,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              image: DecorationImage(
+                image: NetworkImage(widget.image),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Icon(Iconsax.location, color: CustomColor.primary),
+                  SizedBox(width: 8),
+                  Text(
+                    widget.location,
+                    style: TextStyle(color: CustomColor.primary),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Icon(Iconsax.star, color: CustomColor.primary),
+                  SizedBox(width: 8),
+                  Text(widget.rating.toString()),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home__page.dart
+++ lib/screens/home__page.dart
@@ -1,4 +1,6 @@
 import 'package:arifspot/components/location.dart';
+import 'package:arifspot/components/popular__accomodation.dart';
+import 'package:arifspot/constants/color.dart';
 import 'package:flutter/material.dart';
 import 'package:arifspot/components/searchbar.dart' as custom;
 import 'package:arifspot/components/service__tag.dart';
@@ -31,74 +33,122 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            SizedBox(height: 12),
-            custom.SearchBar(),
-            SizedBox(height: 32),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 12),
+              custom.SearchBar(),
+              SizedBox(height: 32),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          // Only select the first ServiceTag and deselect others
+                          isSelected1 = true;
+                          isSelected2 = false;
+                          isSelected3 = false;
+                        });
+                      },
+                      child: ServiceTag(
+                        icon: Iconsax.activity,
+                        name: "Activity",
+                        isSelected: isSelected1,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          // Only select the second ServiceTag and deselect others
+                          isSelected1 = false;
+                          isSelected2 = true;
+                          isSelected3 = false;
+                        });
+                      },
+                      child: ServiceTag(
+                        icon: Iconsax.activity,
+                        name: "Learn with vue",
+                        isSelected: isSelected2,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          // Only select the third ServiceTag and deselect others
+                          isSelected1 = false;
+                          isSelected2 = false;
+                          isSelected3 = true;
+                        });
+                      },
+                      child: ServiceTag(
+                        icon: Iconsax.activity4,
+                        name: "Learn with react",
+                        isSelected: isSelected3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 32),
+              //list location
+              Location(),
+              SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        // Only select the first ServiceTag and deselect others
-                        isSelected1 = true;
-                        isSelected2 = false;
-                        isSelected3 = false;
-                      });
-                    },
-                    child: ServiceTag(
-                      icon: Iconsax.activity,
-                      name: "Activity",
-                      isSelected: isSelected1,
-                    ),
+                  Text(
+                    "Popular Accomodation",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(width: 16),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        // Only select the second ServiceTag and deselect others
-                        isSelected1 = false;
-                        isSelected2 = true;
-                        isSelected3 = false;
-                      });
-                    },
-                    child: ServiceTag(
-                      icon: Iconsax.activity,
-                      name: "Learn with vue",
-                      isSelected: isSelected2,
-                    ),
-                  ),
-                  SizedBox(width: 16),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        // Only select the third ServiceTag and deselect others
-                        isSelected1 = false;
-                        isSelected2 = false;
-                        isSelected3 = true;
-                      });
-                    },
-                    child: ServiceTag(
-                      icon: Iconsax.activity4,
-                      name: "Learn with react",
-                      isSelected: isSelected3,
-                    ),
-                  ),
+                  Text("see all", style: TextStyle(color: CustomColor.primary)),
                 ],
               ),
-            ),
-            SizedBox(height: 32),
-            //list location
-            Location(),
-
-            //popular places
-          ],
+              SizedBox(height: 12),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    PopularAccomodation(
+                      name: "Hotel Sunshine",
+                      location: "California, USA",
+                      rating: 4.5,
+                      image:
+                          "https://img.freepik.com/free-photo/male-chef-preparing-sushi-order-takeaway_23-2149050359.jpg?t=st=1741354218~exp=1741357818~hmac=dd9845508810af459d6b7f5a008b57f6c4c372e34a3cef12d1a1192b86b76666&w=1060",
+                    ),
+                    PopularAccomodation(
+                      name: "Mountain Retreat",
+                      location: "Aspen, Colorado",
+                      rating: 4.8,
+                      image:
+                          "https://img.freepik.com/free-photo/male-chef-preparing-sushi-order-takeaway_23-2149050359.jpg?t=st=1741354218~exp=1741357818~hmac=dd9845508810af459d6b7f5a008b57f6c4c372e34a3cef12d1a1192b86b76666&w=1060",
+                    ),
+                    PopularAccomodation(
+                      name: "City Lights Hotel",
+                      location: "New York, USA",
+                      rating: 4.2,
+                      image:
+                          "https://img.freepik.com/free-photo/male-chef-preparing-sushi-order-takeaway_23-2149050359.jpg?t=st=1741354218~exp=1741357818~hmac=dd9845508810af459d6b7f5a008b57f6c4c372e34a3cef12d1a1192b86b76666&w=1060",
+                    ),
+                    PopularAccomodation(
+                      name: "Beachside Resort",
+                      location: "Miami, Florida",
+                      rating: 4.7,
+                      image:
+                          "https://img.freepik.com/free-photo/male-chef-preparing-sushi-order-takeaway_23-2149050359.jpg?t=st=1741354218~exp=1741357818~hmac=dd9845508810af459d6b7f5a008b57f6c4c372e34a3cef12d1a1192b86b76666&w=1060",
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
